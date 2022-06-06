6d484442aa90fe663ca95241fa64f183be337995
Carlos <andres.agpa@gmail.com>
2022-06-07 01:06:32 +0200

    Modified in pom the version. Fixed url mit DB

--- src/test/java/test/TestMySqlJDBC.java
+++ src/test/java/test/TestMySqlJDBC.java
@@ -4,13 +4,16 @@ import java.sql.*;
 
 public class TestMySqlJDBC {
     public static void main(String[] args) {
-        var url = "jdbc:mysql://localhost:3306/testudemy?SSL=false&useTimezone=true&serverTimeZone=UTC&allowPublicKeyRetrieval=true";
+        var url = "jdbc:mysql://localhost:3306/testudemy?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
         try {
            // Class.forName("com.mysql.cj.jdbc.Driver"); //For Web Apps the most
             Connection connection = DriverManager.getConnection(url,"root", "programmingNow#");
             Statement instruction = connection.createStatement();
             var sql = "SELECT id_person, name, lastname, email, telephone FROM person";
             ResultSet resultSet = instruction.executeQuery(sql);
+            while (resultSet.next()){
+                System.out.println("Id Person: " + resultSet.getInt("id_person"));
+            }
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
